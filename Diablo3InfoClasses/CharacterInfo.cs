b89af5785bcd4aabf5f01af068b12a808e493cc8
Miroslav Lazovic <miroslavlazovic@livenation.com>
2016-06-29 10:55:27 +0200

    Added BasicSkill class to elimiate duplicated code by using inheritance

--- Diablo3InfoClasses/CharacterInfo.cs
+++ Diablo3InfoClasses/CharacterInfo.cs
@@ -65,30 +65,6 @@ namespace Codeforge.Diablo3InfoClasses
         }
 
         [DataContract]
-        public class Skill
-        {
-            [DataMember(Name = "slug")]
-            public string slug { get; set; }
-            [DataMember(Name = "name")]
-            public string name { get; set; }
-            [DataMember(Name = "icon")]
-            public string icon { get; set; }
-            [DataMember(Name = "level")]
-            public int level { get; set; }
-            [DataMember(Name = "categorySlug")]
-            public string categorySlug { get; set; }
-            [DataMember(Name = "tooltipUrl")]
-            public string tooltipUrl { get; set; }
-            [DataMember(Name = "description")]
-            public string description { get; set; }
-            [DataMember(Name = "flavor")]
-            public string flavor { get; set; }
-            [DataMember(Name = "simpleDescription")]
-            public string simpleDescription { get; set; }
-            [DataMember(Name = "skillCalcId")]
-            public string skillCalcId { get; set; }
-        }
-        [DataContract]
         public class Rune
         {
             [DataMember(Name = "slug")]
@@ -115,7 +91,7 @@ namespace Codeforge.Diablo3InfoClasses
         public class Active
         {
             [DataMember(Name = "skill")]
-            public Skill Skill { get; set; }
+            public BasicSkill Skill { get; set; }
             [DataMember(Name = "rune")]
             public Rune Rune { get; set; }
         }
@@ -124,7 +100,7 @@ namespace Codeforge.Diablo3InfoClasses
         public class Passive
         {
             [DataMember(Name = "skill")]
-            public Skill Skill;
+            public BasicSkill Skill;
         }
 
         [DataContract]
--- Diablo3InfoClasses/FollowerInfo.cs
+++ Diablo3InfoClasses/FollowerInfo.cs
@@ -30,45 +30,15 @@ namespace Codeforge.Diablo3InfoClasses
         }
 
         [DataContract]
-        public class Active
+        public class Active : BasicSkill
         {
-            [DataMember(Name = "slug")]
-            public string slug { get; set; }
-            [DataMember(Name = "name")]
-            public string name { get; set; }
-            [DataMember(Name = "icon")]
-            public string icon { get; set; }
-            [DataMember(Name = "level")]
-            public int level { get; set; }
-            [DataMember(Name = "tooltipUrl")]
-            public string tooltipUrl { get; set; }
-            [DataMember(Name = "description")]
-            public string description { get; set; }
-            [DataMember(Name = "simpleDescription")]
-            public string simpleDescription { get; set; }
-            [DataMember(Name = "skillCalcId")]
-            public string skillCalcId { get; set; }
+            
         }
 
         [DataContract]
-        public class Passive
+        public class Passive : BasicSkill
         {
-            [DataMember(Name = "slug")]
-            public string slug { get; set; }
-            [DataMember(Name = "name")]
-            public string name { get; set; }
-            [DataMember(Name = "icon")]
-            public string icon { get; set; }
-            [DataMember(Name = "level")]
-            public int level { get; set; }
-            [DataMember(Name = "tooltipUrl")]
-            public string tooltipUrl { get; set; }
-            [DataMember(Name = "description")]
-            public string description { get; set; }
-            [DataMember(Name = "simpleDescription")]
-            public string simpleDescription { get; set; }
-            [DataMember(Name = "skillCalcId")]
-            public string skillCalcId { get; set; }
+            
         }
     }
 }
